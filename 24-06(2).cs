97b2408c6af7d5c00f8e6a78352140ae53ee9ea2
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-06-24 22:06:23 +0530

    Add files via upload

---
+++ 24-06(2).cs
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+      int a=12;
+      int b=13;
+      int c;
+      c=a+b;
+    Console.WriteLine("the sum of two number: {0}",c);
+    Console.ReadKey();
+  }
+}
