a75a98a1d5c9d618db19d412ecf6a5f1dc48ba7c
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-06-25 00:20:59 +0530

    Add files via upload

---
+++ 24-06(3).cs
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+    
+  
+  static void Main() 
+  {
+       char a = 'G';
+       int i = 55;
+       float f = 5.65f;
+       decimal dec = 345.7m;
+     Console.WriteLine("char: " + a);
+      Console.WriteLine("integer: " + i); 
+       Console.WriteLine("float: " + f);
+        Console.WriteLine("decimal: " + dec);
+  }
+}
