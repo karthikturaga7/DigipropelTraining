6e0964ccd0ef215f6035df9d2323716c1da768a1
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-06-25 19:02:43 +0530

    Add files via upload

---
+++ 25-06(1).cs
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+    
+  static void Main() {
+      int a = 12;
+    int b = 13;
+    int c = 15;
+    int d;
+      d = a*b*c;
+    Console.WriteLine("multiply of three numbers: {0}",d);
+    
+    
+  }
+}
