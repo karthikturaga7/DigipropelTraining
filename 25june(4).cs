1ad664370a3fea1ee95c3ad33f745a3465042d7f
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-06-27 23:30:54 +0530

    Add files via upload

---
+++ 25june(4).cs
@@ -0,0 +1,14 @@
+
+using System;
+
+public class Exercise18 {
+  static void Main(string[] args)
+        {
+           Console.WriteLine("\nInput first integer:");  
+           int x = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Input second integer:");  
+           int y = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Check if one is negative and one is positive:");
+           Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+        }
+}
