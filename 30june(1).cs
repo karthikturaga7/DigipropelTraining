dd6838966e3737927e2c990b5e6a7f4fcdda005b
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-07-05 01:38:38 +0530

    Add files via upload

---
+++ 30june(1).cs
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+//reverse all the words of a given string which have even length.
+using System;
+class Program
+{
+  enum Level
+  {
+    Low,
+    Medium,
+    High,
+  }
+  static void Main(string[] args)
+  {
+    Level myVar = Level.Medium;
+    Console.WriteLine(myVar);
+  }
+}
