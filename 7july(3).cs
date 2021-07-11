ad77c75907142854e732b8c18e2e95574fc626a4
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-07-12 00:37:33 +0530

    Add files via upload

---
+++ 7july(3).cs
@@ -0,0 +1,57 @@
+using System;
+public class Person
+{
+    int age;
+    public void greet()
+    {
+        Console.WriteLine("Hello!");
+    }
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+}
+    public class Student : Person
+    {
+       
+        public void study()
+        {
+            Console.WriteLine("I'm studying on the screen");
+        }
+        public void ShowAge()
+        {
+        Console.WriteLine("My age is: 23 years old on the screen");
+        }
+    }
+    public class Teacher : Person
+    {
+        public void explain()
+        {
+            Console.WriteLine("I'm explaining on the screen");
+        }
+    }
+    public class StudentProfessorTest
+    {
+        public static void Main()
+        {
+            Person obj1 =new Person();
+            obj1.greet();
+           
+           
+            Student obj2 =new Student();
+            obj2.greet();
+            obj2.SetAge(25);
+            obj2.ShowAge();
+            obj2.study();
+           
+            Teacher obj3 =new Teacher();
+            obj3.explain();
+            obj3.SetAge(34);
+            obj3.greet();
+        }
+    }
+    *//Expected Output:
+    I'm studying on the screen
+I'm explaining on the screen
+Hello!
+**//
