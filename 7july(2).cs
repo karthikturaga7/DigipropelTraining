60620241319a3883ffba6a8d48000e8d8055e28c
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-07-12 00:42:07 +0530

    Update 7july(2).cs

--- 7july(2).cs
+++ 7july(2).cs
@@ -1,5 +1,5 @@
 using System;
-public abstract class Animal 
+public abstract class Animal  // using abstract class
 {
 	private string Name;
 	
@@ -11,18 +11,18 @@ public abstract class Animal
 	{
 		return Name ;
 	}
-	public abstract void Eat();
+	public abstract void Eat(); // abstract method
 }
 
 public class AbstractClass
 {
-	public class Dog : Animal
+	public class Dog : Animal // using inhertiance concepts
 {
 public override  void Eat()
 {
 Console.WriteLine("The Dog is Eating ");
 }
-	public static void Main()
+	public static void Main() // main method
 	{
 		Dog obj1 = new Dog();
 		obj1.SetName(Console.ReadLine());
@@ -32,6 +32,11 @@ Console.WriteLine("The Dog is Eating ");
 	}
 }
 }
+*// Expected Output
+	12
+The Dog is Eating 
+**//
+
 
 
    
