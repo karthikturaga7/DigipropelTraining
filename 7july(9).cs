6eb5e707cd6122a7787e913fc8e2647a0cb7f9e4
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-07-12 00:29:33 +0530

    Add files via upload

---
+++ 7july(9).cs
@@ -0,0 +1,24 @@
+﻿using System;  
+                      
+public class CountWordFile  
+{      
+    public static void Main()  
+    {  
+        String line;  
+        int count = 0;  
+          
+        //Opens a file in read mode  
+        System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\user\Desktop\textfile.txt");    
+              
+        //Gets each line till end of file is reached  
+        while((line = file.ReadLine()) != null){  
+            //Splits each line into words  
+            String[] words= line.Split(' ');  
+            //Counts each word  
+            count = count + words.Length;  
+        }  
+          
+        Console.WriteLine("Number of words present in given file: " + count);  
+        file.Close();  
+    }  
+}  
