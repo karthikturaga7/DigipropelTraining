494a4900f3e553ad24340bf771ed2f8650f5c099
karthikturaga7 <86420442+karthikturaga7@users.noreply.github.com>
2021-07-12 01:00:30 +0530

    Add files via upload

---
+++ 7july (9).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+class ReadFriendRecords
+{
+    public static void Main()
+    {
+        //the path of the file
+        FileStream inFile = new FileStream(@"C:\Users\user\Desktop\textfile.txt", FileMode.Open, FileAccess.Read);
+        StreamReader reader = new StreamReader(inFile);
+        string record;
+        string input;
+        Console.Write("Enter the word in the line to  find :\n");
+        input = Console.ReadLine();
+        try
+        {
+            //the program reads the record and displays it on the screen
+            record = reader.ReadLine();
+            while (record != null)
+            {
+                if (record.Contains(input))
+                {
+                    Console.WriteLine(record);
+                }
+               
+                
+                    record = reader.ReadLine();
+                
+            }
+        finally
+        {
+            //after the record is done being read, the progam closes
+            reader.Close();
+            inFile.Close();
+        }
+        
+        
+        Console.ReadLine();
+    }
+}
